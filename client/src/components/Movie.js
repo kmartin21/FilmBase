af5800d8b1e5e9532eba23d1b06b740b059947d0
kmartin21 <kmartin82195@gmail.com>
2018-11-19 16:00:51 -0500

    Added more movie card styling

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -3,18 +3,27 @@ import {Link} from 'react-router-dom'
 import auth0Client from '../containers/oauth/Auth'
 import starEmpty from '../images/star-empty.svg'
 import starFilled from '../images/star-filled.svg'
+import TextEllipsis from 'react-text-ellipsis'
 
 const Movie = ({ id, favorited, favoritedBy, user_id, title, description, imageUrl, isProfile, favoriteMovie, unfavoriteMovie, onClickImage }) => (
     <div className="movie">
-        <img className="movie__favorite-button" src={starFilled} alt='Favorite button' />
+        <img className="movie__favorite-button" src={favorited ? starFilled : starEmpty} alt='Favorite button' onClick={favorited && auth0Client.isAuthenticated() ? () => unfavoriteMovie(id) : () => favoriteMovie(id, title, description, imageUrl)}/>
         <img className="movie__image" src={`https://image.tmdb.org/t/p/w185/${imageUrl}`} alt='Movie image' onClick={() => onClickImage(id)}/>
-        <h5 className="movie__title">{title}</h5>
-        {/* <p>{description}</p> */}
+        <p className="movie__title">{title}</p>
+        {favoritedBy === undefined &&
+            <div className="movie__description">
+                <TextEllipsis 
+                    lines={4} 
+                    tag={'p'} 
+                    ellipsisChars={'...'}>
+                    {description}
+                </TextEllipsis>
+            </div>
+        }
         <div>
             {(favoritedBy !== undefined && !isProfile) &&
-                <p className="movie__description">Favorited by <Link to={`/user/${user_id}/profile`}>{favoritedBy}</Link></p>
+                <p className="movie__favorited-by">Favorited by <Link className="movie__description--link" to={`/user/${user_id}/profile`}>{favoritedBy}</Link></p>
             }
-            <button onClick={favorited && auth0Client.isAuthenticated() ? () => unfavoriteMovie(id) : () => favoriteMovie(id, title, description, imageUrl)}>{favorited ? 'Unfavorite' : 'Favorite'}</button>
         </div> 
     </div>
 )
