60d85df56a1f8ace38e20a380bd240ff4168c810
kmartin21 <kmartin82195@gmail.com>
2018-11-30 11:08:27 -0500

    Added read more button to opinion

--- client/src/containers/MovieDetailsModal.js
+++ client/src/containers/MovieDetailsModal.js
@@ -11,9 +11,12 @@ class MovieDetailsModal extends Component {
         this.state = {
             isEditing: false,
             activeUserOpinion: this.props.activeUserOpinion,
-            collapsed: true,
-            showOverflow: false, 
-            showReadMoreIcon: false
+            descriptionIsCollapsed: true,
+            opinionIsCollapsed: true,
+            descriptionShowOverflow: false,
+            opinionShowOverflow: false,
+            showReadMoreDescriptionIcon: false,
+            showReadMoreOpinionIcon: false,
         }
     }
 
@@ -35,29 +38,41 @@ class MovieDetailsModal extends Component {
         this.setState({ activeUserOpinion: e.target.value })
     }
 
-    setCollapsed = (collapsed) => {
-        this.setState({ collapsed })
+    setDescriptionShowOverflow = () => {
+        this.setState({ descriptionIsCollapsed: false, descriptionShowOverflow: true, showReadMoreDescriptionIcon: false })
     }
 
-    setShowOverflow = () => {
-        this.setState({ collapsed: false, showOverflow: true, showReadMoreIcon: false })
+    setOpinionShowOverflow = () => {
+        this.setState({ opinionIsCollapsed: false, opinionShowOverflow: true, showReadMoreOpinionIcon: false })
     }
 
-    setShowReadMoreIcon = () => {
-        this.setState({ showReadMoreIcon: !this.state.showReadMoreIcon })
+    setShowReadMoreDescriptionIcon = () => {
+        this.setState({ showReadMoreDescriptionIcon: !this.state.showReadMoreDescriptionIcon })
     }
 
-    handleReflow = (rleState) => {
+    setShowReadMoreOpinionIcon = () => {
+        this.setState({ showReadMoreOpinionIcon: !this.state.showReadMoreOpinionIcon })
+    }
+
+    handleDescriptionClamped = (rleState) => {
         const {clamped} = rleState
 
-        if (clamped && !this.state.showReadMoreIcon) {
-            this.setShowReadMoreIcon()
+        if (clamped && !this.state.showReadMoreDescriptionIcon) {
+            this.setShowReadMoreDescriptionIcon()
         }
-      }
+    }
+
+    handleOpinionClamped = (rleState) => {
+        const {clamped} = rleState
+
+        if (clamped && !this.state.showReadMoreOpinionIcon) {
+            this.setShowReadMoreOpinionIcon()
+        }
+    }
 
     render() {
-        const { id, title, imageUrl, description, favorited, favoritedBy, opinion, isActiveUserProfile, onClose, favoriteMovie, unfavoriteMovie, editOpinion } = this.props
-
+        const { id, title, imageUrl, description, favorited, favoritedBy, isActiveUserProfile, onClose, favoriteMovie, unfavoriteMovie, editOpinion } = this.props
+        const opinion = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
         return (
             <div className='movie-details-modal__container'>
                 <a href="#" className="close" onClick={onClose}/>
@@ -65,8 +80,8 @@ class MovieDetailsModal extends Component {
                     <img className="movie-details__image" src={`https://image.tmdb.org/t/p/w500/${imageUrl}`} alt='Movie image'/>
                     <div className="movie-details__info-container">
                     <h5 className="movie-details__title">{title}</h5>
-                        {!this.state.collapsed ?
-                            [(this.state.showOverflow ? 
+                        {!this.state.descriptionIsCollapsed ?
+                            [(this.state.descriptionShowOverflow ? 
                                 <p className="movie-details__description--overflow">{description}</p> :
                                 <p className="movie-details__description">{description}</p>
                             )]
@@ -77,25 +92,51 @@ class MovieDetailsModal extends Component {
                                     maxLine='1'
                                     ellipsis='...'
                                     trimRight
-                                    onReflow={this.handleReflow}
+                                    onReflow={this.handleDescriptionClamped}
                                     basedOn='letters'
                                 />
                             </div>
                         }
-                        
-                        {this.state.showReadMoreIcon &&
-                            <img className="movie-details__more-btn" src={downArrow} alt='Read more button' onClick={() => this.setShowOverflow()}/>
+                         
+                        {this.state.showReadMoreDescriptionIcon &&
+                            <img className="movie-details__more-btn" src={downArrow} alt='Read more button' onClick={() => this.setDescriptionShowOverflow()}/>
                         }
 
-                        {(opinion !== undefined && !isActiveUserProfile) && (
-                            <div>
-                                <h5 className="movie-details__opinion-header">{favoritedBy}'s opinion</h5>
-                                <p className="movie-details__opinion">{opinion}</p>
-                            </div>
-                        )}
+                        {(opinion !== undefined && !isActiveUserProfile) && 
+                            [(!this.state.opinionIsCollapsed) ?
+                                [(this.state.opinionShowOverflow ? 
+                                    <div>
+                                        <h5 className="movie-details__opinion-header">{favoritedBy}'s opinion</h5>
+                                        <p className="movie-details__opinion--overflow">{opinion}</p>
+                                    </div>
+                                     :
+                                    <div> 
+                                        <h5 className="movie-details__opinion-header">{favoritedBy}'s opinion</h5>
+                                        <p className="movie-details__opinion">{opinion}</p>
+                                    </div>
+                                )]
+                                : 
+                                <div>
+                                    <h5 className="movie-details__opinion-header">{favoritedBy}'s opinion</h5>
+                                    <div className="movie-details__opinion">
+                                        <LinesEllipsis
+                                            text={opinion}
+                                            maxLine='1'
+                                            ellipsis='...'
+                                            trimRight
+                                            onReflow={this.handleOpinionClamped}
+                                            basedOn='letters'
+                                        />
+                                    </div>
+                                </div>
+                            ]
+                        }
+
+                        {this.state.showReadMoreOpinionIcon &&
+                            <img className="movie-details__more-btn" src={downArrow} alt='Read more button' onClick={() => this.setOpinionShowOverflow()}/>
+                        }
                     </div>
 
-                    
                     {favorited && (
                         <div>
                             <p>Your opinion: <input type="text" onChange={this.setOpinion} disabled={!this.state.isEditing} defaultValue={this.state.activeUserOpinion}/></p>
@@ -110,3 +151,8 @@ class MovieDetailsModal extends Component {
 }
 
 export default MovieDetailsModal
+
+
+
+
+
