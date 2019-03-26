d73f350204855c3b69f50bad96f2c2f3dbace23e
kmartin4 <keith.martin@nysed.gov>
2019-03-26 11:55:28 -0400

    Bug fix. Added dev env files

--- routes/user.route.js
+++ routes/user.route.js
@@ -13,7 +13,7 @@ const checkJwt = jwt({
         jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
     }),
 
-    audience: `${process.env.AUTH0_AUDIENCE}`,
+    aud: `${process.env.AUTH0_AUDIENCE}`,
     issuer: `https://${process.env.AUTH0_DOMAIN}/`,
     algorithms: ['RS256']
 })
